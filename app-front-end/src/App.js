d7abfb9be98d4c167540334a1fcc20f445570b71
kotagowri <kotagowri2003@gmail.com>
2023-05-02 21:13:08 +0530

    ghgd

--- app-front-end/src/App.js
+++ app-front-end/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginPage from './LoginPage';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
 
-      <login />
+      <LoginPage />
     </div>
   );
 }
