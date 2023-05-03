ea6d2c3afba79a2631bbb4d3e394e8ec581a43e4
kotagowri <kotagowri2003@gmail.com>
2023-05-03 17:30:36 +0530

    gowri

--- app-front-end/src/App.js
+++ app-front-end/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import LoginPage from './LoginPage';
 import './App.css'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+      </Routes>
+      </BrowserRouter>
 
-      <LoginPage />
     </div>
   );
 }
--- app-front-end/src/LoginPage.js
+++ app-front-end/src/LoginPage.js
@@ -1,79 +1,46 @@
 import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
-class LoginPage extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
-
-  handleInputChange = (event) => {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle login logic
-  }
-  const [name,setName] =useState("")
-  const [password,setPassword] =useState("")
+const LoginPage=()=>{
+  const nav=useNavigate();
+  const [name,setName]=useState("")
+  const [password,setPassword]=useState("")
   const Handleclick=async()=>{
-    const  responce = await axios.post("http://localhost:9000/LoginPage/"+name+"/"+password);
+     const responce = await axios.post("http://localhost:9000/LoginPage/"+name+"/"+password);
         console.log(responce.data);
         if(responce.data)
         {
-            alert("successful");
             nav('/main');
         }
         else{
             alert("failed")
         }
-
-
     }
-
-
-  render() {
     return (
-      <div className="login-page">
+      <div >
+        <center>
         <h1>Login to Your Account</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username:</label>
+        
+         <p> Username:</p>
           <input
             type="text"
             id="username"
             name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-            required
-          />
-          <label htmlFor="password">Password:</label>
+            onChange={(e)=>setName(e.target.value)}
+          /><br></br><br></br>
+         <p> Password:</p>
           <input
             type="password"
             id="password"
             name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            required
-          />
+            onChange={(e)=>setName(e.target.value)}
+          /><br></br><br></br><br></br>
           <button onClick={Handleclick}>Login</button>
-      
-        </form>
+          </center>
       </div>
     );
   }
-}
+
 
 export default LoginPage;
